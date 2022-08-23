1769c65b9cae79c56f9966bab7fdadb4c5a5aac8
festuspete <festuspeteragbo@gmail.com>
2022-08-23 18:32:56 +0100

    if conditional statemt

--- Task 1/task.js
+++ Task 1/task.js
@@ -1,3 +1,8 @@
+//Create a for loop expression
 for (let index = 0; index <=15; index++) {
-
+    
+    //An If conditional statement is created
+    if (index % 2 == 0) {
+        const newH1 = document.createElement('h1');
+    }
 }
