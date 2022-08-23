2abb102b9aa458f970de3dba482b62a59644da55
festuspete <festuspeteragbo@gmail.com>
2022-08-23 19:58:30 +0100

    create for loop

--- Task 2/script.js
+++ Task 2/script.js
@@ -1,2 +1,12 @@
 //Create an Array
 let number = [8, 4, 2, 10, 3];
+let largest = 0;
+
+//Create a for loop
+for (let i = 0; i < number.length; i++) {
+    const value = number[i];
+
+    if (value > largest){
+        largest = value;
+    }
+}
