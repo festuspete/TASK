66131de07373dff5de4022d2278b7655b4745e7f
festuspete <festuspeteragbo@gmail.com>
2022-08-23 18:57:39 +0100

    appending child element into DOM

--- Task 1/task.js
+++ Task 1/task.js
@@ -4,5 +4,7 @@ for (let index = 0; index <=15; index++) {
     //An If conditional statement is created
     if (index % 2 == 0) {
         const newH1 = document.createElement('h1');
+        newH1.innerHTML = `${index} is an even number`;
+        document.body.appendChild(newH1);
     }
 }
